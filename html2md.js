605c18b81510b7a2d113a0e186ef22d4ab50e836
chz100p <chz100p@live.jp>
2019-07-16 23:29:54 +0900

    html2md.js

--- html2md.js
+++ html2md.js
@@ -1,46 +1,27 @@
-const commandLineArgs = require('command-line-args');
-
-const optionDefinitions = [
-	  {
-		      name: 'verbose',
-		      alias: 'v',
-		      type: Boolean
-		    },
-	  {
-		      name: 'html',
-		      alias: 'i',
-		      type: String,
-		    },
-	  {
-		      name: 'markdown',
-		      alias: 'o',
-		      type: String,
-		    }
-];
-const options = commandLineArgs(optionDefinitions);
-
-//console.log(options);
-
-if (! options.html) {
-	console.error("require '--html'");
-	process.exit(1);
-}
-
-
-if (! options.markdown) {
-	console.error("require '--markdown'");
-	process.exit(1);
-}
-
-var fs = require('fs');
 var TurndownService = require('turndown');
+var stream = require('stream');
 
-var turndownService = new TurndownService();
+var html2md = new stream.Transform({
+    decodeStrings: false, 
+    encoding: 'utf-8', 
+    transform: function (chunk, encoding, callback) {
+        this.html += chunk;
+        callback(null, '');
+    }, 
+    flush: function (callback) {
+        var turndownService = new TurndownService();
+        var md = turndownService.turndown(this.html);
+        //文字化けしそうな空白文字を置換する
+        md = md.replace(/[ \u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/g , ' ' );
+        this.push(md);
+        callback(null);
+    }
+});
+html2md.html = '';
+html2md.on('error', function (err) {
+    console.error(err);
+    process.exit(1);
+});
 
-var html = fs.readFileSync(options.html, {encoding: 'utf-8'});
-var md = turndownService.turndown(html);
-//文字化けしそうな空白文字を置換する
-md = md.replace(/[ \u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/g , ' ' );
-//console.log(md);
-fs.writeFileSync(options.markdown, md);
+process.stdin.pipe(html2md).pipe(process.stdout);
 
